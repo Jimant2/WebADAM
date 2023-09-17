97e083935c8885e785396b94971772f90d2a145a
Hacklberry <matej.hackl@hotmail.com>
2023-09-17 14:56:15 +0200

    fetch now works

--- reactapp/src/App.jsx
+++ reactapp/src/App.jsx
@@ -6,7 +6,7 @@ export default class App extends Component {
 
     constructor(props) {
         super(props);
-        this.state = { forecasts: [], devices: [], loadingForecasts: true, loadingDevices: true };
+        this.state = { devices: [], loadingDevices: true };
 
     }
 
@@ -16,63 +16,62 @@ export default class App extends Component {
        
     }
 
-    static renderForecastsTable(forecasts) {
-        return (
-            <table className='table table-striped' aria-labelledby="tabelLabel">
-                <thead>
-                    <tr>
-                        <th>Date</th>
-                        <th>Temp. (C)</th>
-                        <th>Temp. (F)</th>
-                        <th>Summary</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {forecasts.map(forecast =>
-                        <tr key={forecast.date}>
-                            <td>{forecast.date}</td>
-                            <td>{forecast.temperatureC}</td>
-                            <td>{forecast.temperatureF}</td>
-                            <td>{forecast.summary}</td>
-                        </tr>
-                    )}
-                </tbody>
-            </table>
-        );
-    }
+    //static renderForecastsTable(forecasts) {
+    //    return (
+    //        <table className='table table-striped' aria-labelledby="tabelLabel">
+    //            <thead>
+    //                <tr>
+    //                    <th>Date</th>
+    //                    <th>Temp. (C)</th>
+    //                    <th>Temp. (F)</th>
+    //                    <th>Summary</th>
+    //                </tr>
+    //            </thead>
+    //            <tbody>
+    //                {forecasts.map(forecast =>
+    //                    <tr key={forecast.date}>
+    //                        <td>{forecast.date}</td>
+    //                        <td>{forecast.temperatureC}</td>
+    //                        <td>{forecast.temperatureF}</td>
+    //                        <td>{forecast.summary}</td>
+    //                    </tr>
+    //                )}
+    //            </tbody>
+    //        </table>
+    //    );
+    //}
 
     static renderDevices(devices) {
-       return <a>Devices: {devices.name}</a>
+       return <a>Devices: {devices[0].deviceName}</a>
     }
 
     render() {
         //let contents = this.state.loading
         //    ? <p><em>Loading... Please refresh once the ASP.NET backend has started. See <a href="https://aka.ms/jspsintegrationreact">https://aka.ms/jspsintegrationreact</a> for more details.</em></p>
         //    : App.renderForecastsTable(this.state.forecasts);
-        let deviceContent = this.state.loadingDevices
-            ? <p>Loading the devices...</p>
-            : App.renderDevices(this.state.devices);
+        let deviceContent = this.state.loadingDevices ? <p>Loading the devices: </p> : App.renderDevices(this.state.devices);
         return (
             <div>
-                <h1 id="tabelLabel" >Weather forecast</h1>
+{/*                <h1 id="tabelLabel" >Weather forecast</h1>*/}
                 <p>This component demonstrates fetching data from the server.</p>
                 {deviceContent}
             </div>
         );
     }
 
-    async populateWeatherData() {
-        const response = await fetch('weatherforecast');
-        const data = await response.json();
-        this.setState({ forecasts: data, loading: false });
-    }
+    //async populateWeatherData() {
+    //    const response = await fetch('weatherforecast');
+    //    const data = await response.json();
+    //    this.setState({ forecasts: data, loading: false });
+    //}
 
     async getDevice() {
         const response = await fetch('https://localhost:7074/MainController/device');
         console.log(response);
         if (response.headers.get("content-type")?.includes("application/json")) {
             const deviceData = await response.json();
-            this.setState({ devices: deviceData, loading: false });
+            console.log(deviceData);
+            this.setState({ devices: deviceData, loadingDevices: false });
         } else {
             console.error('Received non-JSON response:', await response.text());
         }
